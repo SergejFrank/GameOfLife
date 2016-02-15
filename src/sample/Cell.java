46e926220ab225a783741e97c0b091cd27d4490f
Sergej <sergej.frank@otto.de>
2016-02-15 10:05:43 +0100

    my cell

---
+++ src/sample/Cell.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Cell extends Rectangle {
+
+    private static final int SIZE = 20;
+    private boolean isAlive = false;
+
+    public Cell(int pos_x, int pos_y) {
+        super(SIZE, SIZE);
+        setStroke(Color.BLACK);
+        setStrokeWidth(1.0);
+        setFill(Color.WHITE);
+        setPosition(pos_x, pos_y);
+    }
+
+    private void setPosition(int pos_x, int pos_y) {
+        setTranslateX(pos_x * SIZE);
+        setTranslateY(pos_y * SIZE);
+    }
+}
