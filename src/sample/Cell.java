b242a83bc29a1e7433f6f327060996c7e208615d
Sergej <sergej.frank@otto.de>
2016-02-15 10:24:13 +0100

    you can add now cells to the board

--- src/sample/Cell.java
+++ src/sample/Cell.java
@@ -5,19 +5,19 @@ import javafx.scene.shape.Rectangle;
 
 public class Cell extends Rectangle {
 
-    private static final int SIZE = 20;
+    private static final int Size = 15;
     private boolean isAlive = false;
 
     public Cell(int pos_x, int pos_y) {
-        super(SIZE, SIZE);
+        super(Size, Size);
         setStroke(Color.BLACK);
         setStrokeWidth(1.0);
-        setFill(Color.WHITE);
+        setFill(Color.RED);
         setPosition(pos_x, pos_y);
     }
 
     private void setPosition(int pos_x, int pos_y) {
-        setTranslateX(pos_x * SIZE);
-        setTranslateY(pos_y * SIZE);
+        setTranslateX(pos_x * Size);
+        setTranslateY(pos_y * Size);
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,27 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("TEST");
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 250, Color.WHITE);
+
+        Cell c = new Cell(0,0);
+        Cell c2 = new Cell(0,1);
+
+        root.getChildren().add(c);
+        root.getChildren().add(c2);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
